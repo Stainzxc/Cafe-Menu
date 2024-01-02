db7b6d6b7270c915b71fde986a357c547d64f7d7
Ruel A. Almonia <106868902+Stainzxc@users.noreply.github.com>
2024-01-02 16:15:22 +0800

    added image for each flavors

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function showAlert() {
     alert("Normally this link would bring you to another website! It works. This is a testing link.");
 }
+function toggle(flavor) {
+    const menuBlur = document.getElementById("menu-blur");
+    menuBlur.classList.toggle('active');
+    const modal = document.getElementById("modal");
+    const flavorImage = document.getElementById("flavor-image")
+
+    // Set the image source based on the selected flavor
+    if (flavor === 'French Vanilla') {
+        flavorImage.src = "./images/french-vanilla.jpg";
+    } else if (flavor === 'Caramel Macchiato') {
+        flavorImage.src = "./images/caramel-macchiato.jpg";
+    }
+
+    modal.classList.toggle('active');
+}
